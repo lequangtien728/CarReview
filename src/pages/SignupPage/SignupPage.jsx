0366eafe88c9834a035df0fc0c5a7578017a9d58
username <youremail@domain.com>
2022-05-23 13:52:52 -0400

    styling Signup Page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,20 +1,55 @@
 import React, { useState } from "react";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
-
+import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
 import userService from "../../utils/userService";
 import { useNavigate } from "react-router-dom";
 
 export default function SignUpPage(props) {
   return (
-    <>
-      <h1>Signup PAGE</h1>
-      <ul>
-        <li>Read the User Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Signup up func in the User Controller, to know
-          how it is setup to find the user!
-        </li>
-      </ul>
-    </>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          <Image src="https://i.imgur.com/uFR9MGL.jpeg" /> Sign Up
+        </Header>
+        <Form autoComplete="off">
+          <Segment stacked>
+            <Form.Input
+              name="username"
+              placeholder="username"
+              required
+            />
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              required
+            />
+            <Form.Input
+              name="passwordConf"
+              type="password"
+              placeholder="Confirm Password"
+              required
+            />
+            <Form.Field>
+              <Form.Input
+                type="file"
+                name="photo"
+                placeholder="upload image"
+              />
+            </Form.Field>
+            <Button type="submit" className="btn">
+              Signup
+            </Button>
+          </Segment>
+          
+        </Form>
+      </Grid.Column>
+    </Grid>
   );
 }
