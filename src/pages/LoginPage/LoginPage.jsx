e4391188713c6551ec8ca832d8cdd28b5dcba893
username <youremail@domain.com>
2022-05-23 18:29:33 -0400

    adding handleChange function to change email and password state

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,7 +4,20 @@ import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import userService from "../../utils/userService";
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
+
 export default function LoginPage(props) {
+  const [state, setState]=useState({
+    email:"",
+    password:"",
+  });
+
+  function handleChange(e) {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  }
+
   return (
     <>
       <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
@@ -12,18 +25,25 @@ export default function LoginPage(props) {
           <Header as='h2' color='yellow' textAlign='center'>
             <Image src='https://i.imgur.com/uFR9MGL.jpeg' /> Log-in to your account
           </Header>
-          <Form size='large'>
+          <Form size='large' autoComplete="off">
             <Segment stacked>
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
               <Form.Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
+                type="email"
+                name="email"
+                placeholder="email"
+                value={state.email}
+                onChange={handleChange}
+                required
               />
-
-              <Button color='yellow' fluid size='large'>
+              <Form.Input
+                name="password"
+                type="password"
+                placeholder="password"
+                value={state.password}
+                onChange={handleChange}
+                required
+              />
+              <Button color='yellow' fluid size='large' type="submit" className="btn">
                 Login
               </Button>
             </Segment>
