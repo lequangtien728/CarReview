45b267c93f562f641e6c1c4d893a819df8d8438d
username <youremail@domain.com>
2022-05-23 17:39:35 -0400

    adding formData inorder to uploading image

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
-import userService from "../../utils/userService";
-import { useNavigate } from "react-router-dom";
+
+
 
 export default function SignUpPage(props) {
 
@@ -15,9 +15,17 @@ export default function SignUpPage(props) {
     passwordConf:'',
   })
 
+  const [selectedFile, setSelectedFile] = useState('');
 
 
-  function handleSubmit(){
+
+  function handleSubmit(event){
+    event.preventDefault()
+
+    // Create form Data, because we're sending a multipart/formData request, 
+    // because we are sending over multiple requests, because we're uploading a photo!
+    const formData = new FormData(); // new FormData is from the browser
+    formData.append('photo', selectedFile);
 
   }
 
@@ -28,7 +36,9 @@ export default function SignUpPage(props) {
     })
   }
 
-  function handleFileInput(){
+  function handleFileInput(event){
+    console.log(event.target.files);
+    setSelectedFile(event.target.files[0]);
 
   }
 
@@ -38,7 +48,7 @@ export default function SignUpPage(props) {
         <Header as="h2" color="yellow" textAlign="center">
           <Image src="https://i.imgur.com/uFR9MGL.jpeg" /> Sign Up
         </Header>
-        <Form autoComplete="off">
+        <Form autoComplete="off" onSubmit={handleSubmit}>
           <Segment stacked>
             <Form.Input
               name="username"
@@ -83,7 +93,7 @@ export default function SignUpPage(props) {
               Signup
             </Button>
           </Segment>
-          
+          {error ? <ErrorMessage error={error} /> : null}
         </Form>
       </Grid.Column>
     </Grid>
