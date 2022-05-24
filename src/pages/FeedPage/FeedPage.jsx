11c39b549979e71d2bcc6ef2e017eb4617ce3842
username <youremail@domain.com>
2022-05-24 16:56:37 -0400

    adding AddPostForm component to upload car image and add descriptions

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import PageHeader from "../../components/PageHeader/PageHeader";
-import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+// import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import AddPostForm from "../../components/AddPostForm/AddPostForm";
 import * as carsAPI from "../../utils/carApi";
 
 import { Grid } from "semantic-ui-react";
@@ -17,7 +18,7 @@ export default function FeedPage({user,handleLogout}){
       </Grid.Row>
       <Grid.Row>
         <Grid.Column style={{ maxWidth: 450 }}>
-          
+          <AddPostForm/>
         </Grid.Column>
       </Grid.Row>
       <Grid.Row>
