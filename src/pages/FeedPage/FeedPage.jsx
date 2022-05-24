dea877b54d111828a077742718150dae150a0902
username <youremail@domain.com>
2022-05-24 16:42:55 -0400

    Styling PageHeader with link to navigate to Logout, User profilem, and home page

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import PageHeader from "../../components/PageHeader/PageHeader";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import * as carsAPI from "../../utils/carApi";
 
+import { Grid } from "semantic-ui-react";
 
-export default function FeedPage(){
-    return(
-
-    );
-};
+export default function FeedPage({user,handleLogout}){
+  console.log(carsAPI, " <-- carsAPI")
+  
+  return (
+    <Grid centered>
+      <Grid.Row>
+        <Grid.Column>
+          <PageHeader handleLogout={handleLogout} user={user}/>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+}
