c3f0edd0a5e44c6f14e6a46149d1d8a4efddd7df
username <youremail@domain.com>
2022-05-25 14:18:51 -0400

    adding reviews schema to the models

--- models/car.js
+++ models/car.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
+const reviewsSchema = mongoose.Schema({
+  username: String,
+  userId: {type:mongoose.Schema.Types.ObjectId}
+})
+
 
 const carSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}, // referencing a model
     photoUrl: String,
     caption: String,
-    
+    reviews: [reviewsSchema] //embedded Schema
   })
  
 
